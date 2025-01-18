3f27e7a16e7c5a367b91d0deded0b062b78f5f06
Aniket kadukar <aniketdkadukar@gmail.com>
2025-01-18 17:39:56 +0530

    Implement Edit & Delete Task Functionality
    - Implement Edit Functionality
    - Created Edit_Task view, url and template.
    - Implemented Delete Task Functionality.
    - Created Delete Task view and url.

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -6,4 +6,7 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_task/', views.add_task, name='add_task'),
+    path('edit_task/<int:task_id>', views.edit_task, name='edit_task'),
+    path('delete_task/<int:task_id>', views.delete_task, name='delete_task'),
+    path('mark_completed/<int:task_id>', views.mark_completed, name='mark_completed'),
 ]
--- src/todo/views.py
+++ src/todo/views.py
@@ -15,6 +15,33 @@ def add_task(request):
         if form.is_valid():
             form.save()
             return redirect('todo:index')
+        # Don't overwrite the form if it's invalid
+        return render(request, 'todo/add_task.html', {'form': form})
+
+    # Render an empty form for GET requests
     form = AddTaskForm()
-    context = {'form': form}
-    return render(request, 'todo/add_task.html', context=context)
+    return render(request, 'todo/add_task.html', {'form': form})
+
+def edit_task(request, task_id):
+    task = Task.objects.get(pk = task_id)
+    form = AddTaskForm(instance= task)
+    if request.method == "POST":
+        form = AddTaskForm(request.POST, instance= form)
+        if form.is_valid():
+            form.save()
+            return redirect('todo:index')
+    return render(request, 'todo/edit_task.html', {'form' : form})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(pk = task_id)
+    task.delete()
+    return redirect('todo:index')
+
+def mark_completed(request, task_id):
+    if request.method == 'GET':
+        task = Task.objects.get(task_id)
+        print(request.POST)
+    return redirect('todo:index')
+
+
+        
